9709d1e97f086d2c02af907a67fa406925249e2c
18657 <18657@18657.com>
2021-09-01 22:28:35 +0800

    add quickSort 右移法

--- Chapter11/quick_sort/quick_sort.go
+++ Chapter11/quick_sort/quick_sort.go
@@ -4,7 +4,7 @@ import "fmt"
 
 var a = [8]int{5, 2, 4, 7, 1, 3, 2, 6}
 
-func partition(start int, end int) int {
+func partitionLeft(start int, end int) int {
 	pivot := start
 	index := pivot + 1
 
@@ -18,9 +18,26 @@ func partition(start int, end int) int {
 	return index - 1
 }
 
+func partitionRight(start int, end int) int {
+	pivot := start
+	for start < end {
+		for a[pivot] <= a[end] && start < end {
+			end--
+		}
+		for a[start] <= a[pivot] && start < end {
+			start++
+		}
+		if start < end {
+			a[start], a[end] = a[end], a[start]
+		}
+	}
+	a[pivot], a[end] = a[end], a[pivot]
+	return end
+}
+
 func quickSort(start int, end int) {
 	if start < end {
-		mid := partition(start, end)
+		mid := partitionRight(start, end)
 		fmt.Printf("partition (%d-%d, mid=%d) %d %d %d %d %d %d %d %d\n",
 			start, end, mid,
 			a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7])
